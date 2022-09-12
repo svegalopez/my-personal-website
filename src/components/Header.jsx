7794c504cd0349a6be5ab15d01f164107d5fdf3e
Sebastian Vega <svegalopez@gmail.com>
2022-09-12 14:30:46 -0400

    added footer and enabled links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 export default function Header() {
   return (
@@ -7,24 +8,7 @@ export default function Header() {
         <h2>Sebastian Vega</h2>
       </Link>
 
-      {/* desktop nav: make reusable in footer */}
-      <nav className="header__desktop-nav">
-        <Link className="header__link" to="/my-work">
-          <span className="highlight">Projects</span>
-        </Link>
-        <a>
-          <img
-            className="header__desktop-nav-icon"
-            src={process.env.PUBLIC_URL + "/linkedin.svg"}
-          />
-        </a>
-        <a>
-          <img
-            className="header__desktop-nav-icon"
-            src={process.env.PUBLIC_URL + "/github.svg"}
-          />
-        </a>
-      </nav>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar({ hideProjects }) {
+  return (
+    <nav className="header__desktop-nav">
+      {hideProjects || (
+        <Link className="header__link" to="/my-work">
+          <span className="highlight">Projects</span>
+        </Link>
+      )}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/sebastian-vega-513618230"
+      >
+        <img
+          alt=""
+          className="header__desktop-nav-icon"
+          src={process.env.PUBLIC_URL + "/linkedin.svg"}
+        />
+      </a>
+      <a target="_blank" rel="noreferrer" href="https://github.com/svegalopez">
+        <img
+          alt=""
+          className="header__desktop-nav-icon"
+          src={process.env.PUBLIC_URL + "/github.svg"}
+        />
+      </a>
+    </nav>
+  );
+}
