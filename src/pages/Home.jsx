f9ee93649014ca4347ebb4f66f9ad2f0d886bdf8
Sebastian Vega <svegalopez@gmail.com>
2022-09-16 18:06:04 -0400

    redesigned

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,50 +4,11 @@ export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <>
-      <section className="hero">
-        <div className="hero__heading">
-          <h1>
-            {/* Link to home */}I am{" "}
-            <span className="highlight">Sebastian</span>
-          </h1>
-          <h1>I build Software</h1>
-        </div>
-      </section>
-
-      <section className="home-image"></section>
-
-      <section className="home-copy">
-        <p>
-          I am an enthusiastic full stack developer with 8 years of experience
-          solving problems in various industries including consumer electronics,
-          aviation, and medical device.
-        </p>
-        <p>
-          My strongest skills are around node.js, react, SQL, typescript,
-          API-development, testing, jest, cypress, event-driven systems, and
-          AWS.
-        </p>
-        <p>
-          My proudest achievement is to have contributed over 70 Pull Requests
-          to{" "}
-          <a
-            className="home-copy__link"
-            target="_blank"
-            rel="noreferrer"
-            href="https://proknow.com"
-          >
-            ProKnow
-          </a>
-          , currently used in various large institutions around world.
-        </p>
-      </section>
-
-      <section className="home-cta">
-        <button onClick={() => navigate("/projects")} className="home-cta__btn">
-          Projects
-        </button>
-      </section>
-    </>
+    <div className="page">
+      <div className="hero">
+        <div className="hero__img"></div>
+        <div className="hero__heading"></div>
+      </div>
+    </div>
   );
 }
