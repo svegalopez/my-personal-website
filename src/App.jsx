bcb401bfd93a498f4f51c4406267a5a67aeebc03
Sebastian Vega <svegalopez@gmail.com>
2022-09-16 13:35:51 -0400

    moved App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import "../styles/App.css";
-import Home from "../pages/Home";
-import MyWork from "../pages/MyWork";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import "./styles/App.css";
+import Home from "./pages/Home";
+import MyWork from "./pages/MyWork";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
