ef7b66a3eee2bfc741760d86d2e41f105f6aedf4
Sebastian Vega <svegalopez@gmail.com>
2022-09-20 10:38:42 -0400

    added icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import "./Footer.scss";
+import { GitHub, Linkedin } from "react-feather";
 
 export default function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <GitHub className="icon" color="white" />
+      <Linkedin className="icon" color="white" />
+    </div>
+  );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,11 @@
 import "./Header.scss";
+import { GitHub, Linkedin } from "react-feather";
 
 export default function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <GitHub className="icon" color="white" />
+      <Linkedin className="icon" color="white" />
+    </div>
+  );
 }
