85abf6ce97c2ab6367e9a21117f8988601652375
Sebastian Vega <svegalopez@gmail.com>
2022-09-20 10:43:41 -0400

    updates

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,15 @@ import { GitHub, Linkedin } from "react-feather";
 export default function Header() {
   return (
     <div className="header">
-      <GitHub className="icon" color="white" />
-      <Linkedin className="icon" color="white" />
+      <a href="https://github.com/svegalopez" target="_blank">
+        <GitHub className="icon" color="white" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/sebastian-vega-513618230"
+        target="_blank"
+      >
+        <Linkedin className="icon" color="white" />
+      </a>
     </div>
   );
 }
